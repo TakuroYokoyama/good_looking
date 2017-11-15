f031f80cc08d20cd46ae6fccb7718166ac276385
横山拓郎 <t.yokoyama.slj@gmail.com>
2017-11-15 23:37:25 +0900

    ハート位置のピクセルが小数になってしまう場合があるので、roundで四捨五入

--- webroot/js/post.js
+++ webroot/js/post.js
@@ -24,10 +24,10 @@ document.getElementById("target").addEventListener("click", function(e) {
 	$myIMG = document.getElementById("myIMG")
 	$myIMG.style.left = (mouseX - 9) + 'px';
 	$myIMG.style.top = (mouseY - 7) + 'px';
-	document.getElementById("roc_x").value = offsetX - 9;
-	document.getElementById("roc_y").value = offsetY - 7;
-	console.log(document.getElementById("roc_x").value);
-	console.log(document.getElementById("roc_y").value);
+	document.getElementById("roc_x").value = Math.round(offsetX - 9);
+	document.getElementById("roc_y").value = Math.round(offsetY - 7);
+	console.log("roc_x"+document.getElementById("roc_x").value);
+	console.log("roc_y"+document.getElementById("roc_y").value);
 } ) ;
 
 function postValue() {
