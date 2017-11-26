eb99145081db6578c1ac0b9a0d926a995972cf6f
SLJYusukeSuzuki <baskepengin@gmail.com>
2017-11-26 14:01:20 +0900

    ログイン画面作成 ・index.ctpをlogin.ctpに名前変更 ・遷移先グラフ画面を作成

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,12 +3,35 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 
-class ClientController extends AppController
-{
+class ClientController extends AppController{
+    public function initialize(){
+        $this->name = "Client";
+        $this->autoRender = true;
+        $this->viewBuilder()->autoLayout(false);
+    }
 
-    public function index()
-        {
+    public function index() {  
+        return $this->redirect(['action' => "login"]);
+    }
 
-        }
+    public function login() {  
+        $id = $this->request->data('id');
+        $pass = $this->request->data('pass');
+        
+        //ログインできるユーザ・パスワードを固定
+        $idAdmin = "administrator";
+        $passAdmin = "admin";
 
+        if($this->request->is('post')){
+            if( strcmp($id,$idAdmin) == 0 && strcmp($pass,$passAdmin) == 0){
+                return $this->redirect(['action' => "aggregate"]);
+             }else{
+                $this->set("loginErrMessage","ログイン失敗");
+             }
+         }
+    }
+
+    public function aggregate() {  
+        
+    }
 }
