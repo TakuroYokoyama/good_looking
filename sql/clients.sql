9f6bc5258f9e09ab3765a6e48ec8dcba1685904a
横山拓郎 <t.yokoyama.slj@gmail.com>
2018-02-18 17:30:44 +0900

    Revert "clients.sqlを更新"
    This reverts commit 161ef34574acfa5936f7c60e124fcbda57b87dba.

--- sql/clients.sql
+++ sql/clients.sql
@@ -1,75 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.7.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: 2018 年 2 月 18 日 11:53
--- サーバのバージョン： 10.1.26-MariaDB
--- PHP Version: 7.1.8
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `good_looking`
---
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `clients`
---
+use good_looking;
 
 CREATE TABLE `clients` (
-  `person_no` int(3) NOT NULL,
-  `name_initial` varchar(5) NOT NULL,
-  `press_return` int(4) DEFAULT NULL,
-  `img_name` varchar(255) NOT NULL,
-  `del_flg` tinyint(1) NOT NULL
+	`person_no` int(3) NOT NULL,
+	`name_initial` varchar(5) NOT NULL,
+	`press_return` int(4),
+	`del_flg` boolean NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- テーブルのデータのダンプ `clients`
---
-
-INSERT INTO `clients` (`person_no`, `name_initial`, `press_return`, `img_name`, `del_flg`) VALUES
-(1, 't_k', NULL, '1', 0),
-(2, 't_k', NULL, '1', 0),
-(3, 't_k', NULL, '1', 0),
-(4, 't_k', NULL, '1', 0),
-(5, 't_k', NULL, '1', 0),
-(6, 't_k', NULL, '1', 0),
-(7, 'aa', NULL, 'aa', 0),
-(8, 'a', NULL, 'a', 0);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `clients`
---
 ALTER TABLE `clients`
-  ADD PRIMARY KEY (`person_no`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `clients`
---
-ALTER TABLE `clients`
-  MODIFY `person_no` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+	ADD PRIMARY KEY (`person_no`);
