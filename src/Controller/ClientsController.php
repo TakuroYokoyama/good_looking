4011cca9973297087ed9d421225de6d790a7fe52
梅本将太 <u.syouta@gmail.com>
2018-02-25 17:42:04 +0900

    ConnectionManagerを追加

--- src/Controller/ClientsController.php
+++ src/Controller/ClientsController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Datasource\ConnectionManager;
 
 class ClientsController extends AppController{
     public function initialize(){
